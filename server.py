578cacb5e3096701b2372f6e196db8be95b4cfd2
Nabil <nabilaltamash@gmail.com>
2022-11-05 23:55:30 +0600

    Getting started with Fast API

---
+++ server.py
@@ -0,0 +1,38 @@
+# Python commands to install the following modules
+# fastapi uvicorn
+# pip install fastapi
+# pip install uvicorn
+# How to run the project
+# uvicorn server:app --reload
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get('/')
+def home():
+    return {'name': 'nabil'}
+
+
+titans = ['Tim Dodd', 'Dick Grayson']
+
+
+@app.get('/titans')
+def getProducts():
+    return titans
+
+
+@app.get('/titans/{id}')
+def getOneProduct(id: int):
+    if (id > len(titans) - 1):
+        return 'Out of range!'
+
+    return titans[id]
+
+# HTTP status code
+# 2xx, 3xx, 4xx, 5xx
+# 2xx: Okay
+# 3xx: Redirect
+# 4xx: There was an error from the client
+# 5xx: There was an error on the server
