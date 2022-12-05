81b3b43b148d955ea58ca2e1e50c8b6eb154e0bb
Tamim Ahamed <tamimssgts999@gmail.com>
2022-12-05 20:41:20 +0600

    done

--- server.py
+++ server.py
@@ -40,31 +40,105 @@ def getOneProduct(id: int):
 # Write these routes:
 
 # POST /login
+@app.post('/login')
+def getLogin():
+    pass
+
 # POST /login/manager
+@app.post('/login/manager')
+def postLoginManger():
+    pass
+
 # POST /login/employee
+@app.post('/login/employee')
+def postLoginManager():
+    pass
+
 
 # GET /profile
+@app.get('/profile')
+def getProfile():
+    pass
+
 # PATCH /profile
+@app.patch('/profile')
+def patchProfile():
+    pass
 
 # GET /profile/manager
+@app.get('/profile/manager')
+def getProfileManager():
+    pass
+
 # PATCH /profile/manager
+@app.patch('/profile/manager')
+def patchProfileManager():
+    pass
 
 # GET /profile/employee
+@app.get('/profile/employee')
+def getProfileEmployee():
+    pass
+
 # PATCH /profile/employee
+@app.patch('/profile/employee')
+def patchProfileEmployee():
+    pass
 
 # GET /employees
+@app.get('/employees')
+def getEmployees():
+    pass
+
 # GET /employees/{id}
+@app.get('/employees/{id}')
+def getEmployeesId():
+    pass
+
 # UPDATE /employees/{id}
+@app.update('/employees/{id}')
+def updateEmployeesId():
+    pass
+
 # DELETE /employees/{id}
+@app.delete('/employees/{id}')
+def deleteEmployeesId():
+    pass
 
 # POST /activities
+@app.post('/activities')
+def postActivities():
+    pass
+
 # GET /activities
+@app.get('/activities')
+def getActivities():
+    pass
+
 # GET /activities/{id}
+@app.get('/activities/{id}')
+def getActivitiesId():
+    pass
+
 # PATCH /activities/{id}
+@app.patch('/activities/{id}')
+def patchActivitiesId():
+    pass
+
 # DELETE /activities/{id}
+@app.delete('/activities/{id}')
+def deleteActivitiesId():
+    pass
 
 # GET /companies/{id}
+@app.get('/companies/{id}')
+def getCompaniesId():
+    pass
+
 # PATCH /companies/{id}
+@app.patch('/companies/{id}')
+def patchCompaniesId():
+    pass
 
 # =======================
 
